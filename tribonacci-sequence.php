8cdad7db0826f2cb696c964c2edcf472a174b74a
Pr0C33D <hero1393@gmail.com>
2021-10-11 12:41:56 +0300

    DONE

---
+++ tribonacci-sequence.php
@@ -0,0 +1,10 @@
+<?php 
+// https://www.codewars.com/kata/556deca17c58da83c00002db
+
+function tribonacci($s, $n) {
+ for ($i=0;count($s)<$n;$i++) {
+  	$s[] = $s[$i]+$s[$i+1]+$s[$i+2];
+  }
+  return array_splice($s, 0, $n);
+}
+?>
